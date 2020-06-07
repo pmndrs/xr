08787b092bdd2e5ece5cd5fc948a92ecb87a1c62
Oleksandr Dubenko <dubenko.oleksandr@gmail.com>
2020-06-07 14:45:07 +0200

    Add RayGrab interaction

--- src/Interactions.tsx
+++ src/Interactions.tsx
@@ -1,7 +1,8 @@
-import React, { useRef, useEffect, useCallback } from 'react'
+import React, { useRef, useEffect, useCallback, ReactNode } from 'react'
 import { useXR, useXREvent, XRInteractionEvent, XREvent } from './XR'
 import { XRHandedness } from './webxr'
-import { Object3D } from 'three'
+import { Object3D, Group, Matrix4 } from 'three'
+import { useFrame } from 'react-three-fiber'
 
 export function Hover({ onChange, children }: any) {
   const ref = useRef<Object3D>()
@@ -53,4 +54,55 @@ export function Select({ onSelect, children }: any) {
   }, [addInteraction])
 
   return <group ref={ref}>{children}</group>
+}
+
+export function RayGrab({ children }: { children: ReactNode }) {
+  const { addInteraction } = useXR()
+
+  const hoveredHandedness = useRef<Set<XRHandedness | undefined>>(new Set())
+  const grabbingController = useRef<Object3D>()
+  const groupRef = useRef<Group>()
+
+  const previousTransform = useRef<Matrix4 | undefined>(undefined)
+
+  const onEnd = useCallback((_: XREvent) => {
+    grabbingController.current = undefined
+    previousTransform.current = undefined
+  }, [])
+
+  const onStart = useCallback((e: XREvent) => {
+    if (hoveredHandedness.current.has(e.controller.inputSource?.handedness)) {
+      grabbingController.current = e.controller.controller
+      previousTransform.current = new Matrix4().getInverse(e.controller.controller.matrixWorld)
+    }
+  }, [])
+
+  useXREvent('selectstart', onStart)
+  useXREvent('selectend', onEnd)
+
+  useFrame(() => {
+    if (!grabbingController.current || !previousTransform.current || !groupRef.current) {
+      return
+    }
+
+    const controller = grabbingController.current
+    const group = groupRef.current
+
+    group.applyMatrix4(previousTransform.current)
+    group.applyMatrix4(controller.matrixWorld)
+    group.updateWorldMatrix(false, true)
+
+    previousTransform.current.getInverse(controller.matrixWorld)
+  })
+
+  useEffect(() => {
+    addInteraction(groupRef.current as Object3D, 'onHover', (e: XRInteractionEvent) => {
+      hoveredHandedness.current.add(e.controller.inputSource?.handedness)
+    })
+    addInteraction(groupRef.current as Object3D, 'onBlur', (e: XRInteractionEvent) => {
+      hoveredHandedness.current.delete(e.controller.inputSource?.handedness)
+    })
+  }, [addInteraction])
+
+  return <group ref={groupRef}>{children}</group>
 }
