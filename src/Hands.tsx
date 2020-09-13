699b74cca8341380ce563f885866eaaf7163008b
Oleksandr Dubenko <dubenko.oleksandr@gmail.com>
2020-09-13 16:37:17 +0200

    Add profile option to Hands component

--- src/Hands.tsx
+++ src/Hands.tsx
@@ -3,21 +3,28 @@ import { useEffect } from 'react'
 // @ts-ignore
 import { XRHandModelFactory } from 'three/examples/jsm/webxr/XRHandModelFactory'
 
-export function Hands() {
+interface HandsProps {
+  profile?: 'spheres' | 'boxes' | 'oculus' | 'oculus_lowpoly'
+}
+
+export function Hands({ profile = 'oculus' }: HandsProps) {
   const { scene, gl } = useThree()
 
   useEffect(() => {
     const handFactory = new XRHandModelFactory().setPath('https://threejs.org/examples/models/fbx/')
 
+    const options = profile === 'oculus_lowpoly' ? { model: 'lowpoly' } : undefined
+    const threeProfile = profile === 'oculus_lowpoly' ? 'oculus' : profile
+
     // @ts-ignore
     const hand1 = gl.xr.getHand(0)
     scene.add(hand1)
-    hand1.add(handFactory.createHandModel(hand1, 'oculus'))
+    hand1.add(handFactory.createHandModel(hand1, threeProfile, options))
 
     // @ts-ignore
     const hand2 = gl.xr.getHand(1)
     scene.add(hand2)
-    hand2.add(handFactory.createHandModel(hand2, 'oculus'))
+    hand2.add(handFactory.createHandModel(hand2, threeProfile, options))
   }, [scene, gl])
 
   return null
