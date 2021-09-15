35db4e97557fab089eabd18b2f7f6a2b85eca5f3
Michael Bashurov <accounts@saitonakamura.me>
2021-09-15 12:32:05 +0300

    Added console warning when XR context isn't initialized
    Previously there was an error "addInteraction is not a function" when trying to use interaction without AR/VR Canvas

--- src/Interactions.tsx
+++ src/Interactions.tsx
@@ -24,11 +24,17 @@ export type XRInteractionType =
 
 export type XRInteractionHandler = (event: XRInteractionEvent) => any
 
+const warnAboutVRARCanvas = () => console.warn('You must provide a ARCanvas or VRCanvas as a wrapper to use interactions')
+
 export const InteractionsContext = React.createContext<{
   hoverState: Record<XRHandedness, Map<Object3D, Intersection>>
   addInteraction: (object: Object3D, eventType: XRInteractionType, handler: XRInteractionHandler) => any
   removeInteraction: (object: Object3D, eventType: XRInteractionType, handler: XRInteractionHandler) => any
-}>({} as any)
+}>({
+  hoverState: {} as any,
+  addInteraction: warnAboutVRARCanvas,
+  removeInteraction: warnAboutVRARCanvas
+})
 export function InteractionManager({ children }: { children: any }) {
   const { controllers } = useXR()
 
