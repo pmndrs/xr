200d16f3b1886edfc99bbb38a9e26c4963565ac9
Bela Bohlender <bbohlender@coconut-capital.com>
2021-09-24 12:20:06 +0200

    added ExitXRButton

--- src/XRButton.tsx
+++ src/XRButton.tsx
@@ -1,12 +1,20 @@
-import React, { ButtonHTMLAttributes } from 'react'
-import { forwardRef } from 'react'
+import React, { forwardRef, ButtonHTMLAttributes } from 'react'
 import { XRSessionMode } from 'webxr'
+import { useExitXRSession } from 'XRSessionManager'
 import { useRequestXRSession } from '.'
 
-export const XRButton = forwardRef<
+export const EnterXRButton = forwardRef<
   HTMLButtonElement,
   ButtonHTMLAttributes<HTMLButtonElement> & { sessionMode: XRSessionMode; sessionInit?: any }
 >(({ sessionMode, sessionInit, ...props }, ref) => {
   const enterSession = useRequestXRSession()
-  return <button ref={ref} {...props} onClick={() => enterSession(sessionMode, sessionInit)} />
+  return <button ref={ref} {...props} onClick={() => enterSession(sessionMode, sessionInit).catch(console.error)} />
+})
+
+export const ExitXRButton = forwardRef<
+  HTMLButtonElement,
+  ButtonHTMLAttributes<HTMLButtonElement> & { sessionMode: XRSessionMode; sessionInit?: any }
+>((props, ref) => {
+  const exitSession = useExitXRSession()
+  return <button ref={ref} {...props} onClick={() => exitSession().catch(console.error)} />
 })
