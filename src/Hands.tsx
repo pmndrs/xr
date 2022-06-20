1a83ec06f1259a69e0c91b4b797debcceb821fb8
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2022-06-20 00:47:26 -0500

    refactor(Hands): mount in R3F

--- src/Hands.tsx
+++ src/Hands.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { useThree } from '@react-three/fiber'
+import { createPortal } from '@react-three/fiber'
 import { OculusHandModel } from 'three-stdlib'
 import { useXR } from './XR'
 
@@ -7,35 +7,20 @@ export interface HandsProps {
   modelLeft?: string
   modelRight?: string
 }
-export function Hands(props: HandsProps) {
-  const scene = useThree((state) => state.scene)
-  const gl = useThree((state) => state.gl)
+export function Hands({ modelLeft, modelRight }: HandsProps) {
   const controllers = useXR((state) => state.controllers)
+  const handModels = React.useMemo(
+    () => controllers.map(({ hand }) => [hand, new OculusHandModel(hand, modelLeft, modelRight)]),
+    [controllers, modelLeft, modelRight]
+  )
 
-  React.useEffect(() => {
-    controllers.forEach(({ hand, inputSource }) => {
-      const handModel = hand.children.find((child) => child instanceof OculusHandModel) as OculusHandModel | undefined
-      if (handModel) {
-        hand.remove(handModel)
-        handModel.dispose()
-      }
+  // Dispatch fake connected event to start loading models on mount
+  React.useEffect(
+    () => void controllers.forEach(({ hand, inputSource }) => hand.dispatchEvent({ type: 'connected', data: inputSource, fake: true })),
+    [controllers]
+  )
 
-      hand.add(new OculusHandModel(hand, props.modelLeft, props.modelRight))
-
-      // throwing fake event for the Oculus Hand Model so it starts loading
-      hand.dispatchEvent({ type: 'connected', data: inputSource, fake: true })
-    })
-
-    return () => {
-      controllers.forEach(({ hand }) => {
-        const handModel = hand.children.find((child) => child instanceof OculusHandModel) as OculusHandModel | undefined
-        if (handModel) {
-          hand.remove(handModel)
-          handModel.dispose()
-        }
-      })
-    }
-  }, [scene, gl, controllers, props.modelLeft, props.modelRight])
-
-  return null
+  return handModels.map(([hand, model], i) => (
+    <React.Fragment key={`hand-${i}`}>{createPortal(<primitive object={model} />, hand)}</React.Fragment>
+  ))
 }
