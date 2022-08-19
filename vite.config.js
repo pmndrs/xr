6dabe84692ef20f6e8b70304859c079a80063e31
Cody Bennett <hi@codyb.co>
2022-08-19 08:29:53 -0500

    fix: regenerate types on build (#175)

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,36 @@
 import path from 'path'
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-const dev = defineConfig({
-  root: 'examples',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@react-three/xr': path.resolve(process.cwd(), 'src')
-    }
-  }
-})
-
-const build = defineConfig({
-  build: {
-    minify: false,
-    sourcemap: true,
-    target: 'es2018',
-    lib: {
-      formats: ['es', 'cjs'],
-      entry: 'src/index.tsx',
-      fileName: '[name]'
-    },
-    rollupOptions: {
-      external: (id) => !id.startsWith('.') && !path.isAbsolute(id),
-      output: {
-        preserveModules: true,
-        sourcemapExcludeSources: true
+const config = {
+  serve: {
+    root: 'examples',
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@react-three/xr': path.resolve(process.cwd(), 'src')
       }
     }
   },
-  plugins: [
-    {
-      generateBundle() {
-        this.emitFile({ type: 'asset', fileName: 'index.d.ts', source: `export * from '../src'` })
+  build: {
+    build: {
+      minify: false,
+      sourcemap: true,
+      target: 'es2018',
+      lib: {
+        formats: ['es', 'cjs'],
+        entry: 'src/index.tsx',
+        fileName: '[name]'
+      },
+      rollupOptions: {
+        external: (id) => !id.startsWith('.') && !path.isAbsolute(id),
+        output: {
+          preserveModules: true,
+          sourcemapExcludeSources: true
+        }
       }
     }
-  ]
-})
+  }
+}
 
-export default process.argv[2] ? build : dev
+export default defineConfig(({ command }) => config[command])
