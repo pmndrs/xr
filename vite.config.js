8d8250d1a860236017334e779dd8fec91411b488
Don McCurdy <dm@donmccurdy.com>
2022-11-09 13:35:34 -0500

    chore: set up SSL for development environment (#212)

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
+import ssl from '@vitejs/plugin-basic-ssl'
 import { defineConfig } from 'vite'
 
 const config = {
   serve: {
     root: 'examples',
-    plugins: [react()],
+    plugins: [react(), ssl()],
+    server: { host: true, https: true },
     resolve: {
       alias: {
         '@react-three/xr': path.resolve(process.cwd(), 'src')
