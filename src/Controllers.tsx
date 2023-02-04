4eb93922b80b5607a1c0e679f55831244a53abc5
Michael Bashurov <accounts@saitonakamura.me>
2023-02-04 15:18:42 +0200

    Vendor and fix XRControllerModelFactory

--- src/Controllers.tsx
+++ src/Controllers.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import * as THREE from 'three'
-import { XRControllerModelFactory } from 'three-stdlib'
 import { useFrame, Object3DNode, extend, createPortal } from '@react-three/fiber'
 import { useXR } from './XR'
 import { XRController } from './XRController'
 import { useIsomorphicLayoutEffect } from './utils'
+import { XRControllerModel, XRControllerModelFactory } from './XRControllerModelFactory'
+import { XRControllerEvent } from './XREvents'
 
 export interface RayProps extends Partial<JSX.IntrinsicElements['object3D']> {
   /** The XRController to attach the ray to */
@@ -46,9 +47,33 @@ export const Ray = React.forwardRef<THREE.Line, RayProps>(function Ray({ target,
 const modelFactory = new XRControllerModelFactory()
 
 class ControllerModel extends THREE.Group {
+  readonly target: XRController
+  readonly xrControllerModel: XRControllerModel
+
   constructor(target: XRController) {
     super()
-    this.add(modelFactory.createControllerModel(target.controller))
+    this.xrControllerModel = new XRControllerModel()
+    this.target = target
+    this.add(this.xrControllerModel)
+
+    this._onConnected = this._onConnected.bind(this)
+    this._onDisconnected = this._onDisconnected.bind(this)
+
+    this.target.controller.addEventListener('connected', this._onConnected)
+    this.target.controller.addEventListener('disconnected', this._onDisconnected)
+  }
+
+  private _onConnected(event: XRControllerEvent) {
+    modelFactory.initializeControllerModel(this.xrControllerModel, event)
+  }
+
+  private _onDisconnected(_event: XRControllerEvent) {
+    this.xrControllerModel.disconnect()
+  }
+
+  dispose() {
+    this.target.controller.removeEventListener('connected', this._onConnected)
+    this.target.controller.removeEventListener('disconnected', this._onDisconnected)
   }
 }
 
