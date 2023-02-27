a30e5dd863e6c69bf7a6cd9347181aff3c1a9fb8
Michael サイトー 中村 Bashurov <accounts@saitonakamura.me>
2023-02-27 12:10:01 +0200

    Started with unit testing (#243)

---
+++ src/XR.test.tsx
@@ -0,0 +1,90 @@
+import { describe, it, expect, beforeEach, afterEach, vi } from 'vitest'
+import { act, create, ReactTestRenderer, ReactTestRendererJSON } from 'react-test-renderer'
+import { XRButton } from './XR'
+import * as React from 'react'
+import { XRSystemMock } from './mocks/XRSystemMock'
+
+/**
+ * Got this from vitest react example
+ * @see https://vitest.dev/guide/#examples 
+ */
+function toJson(component: ReactTestRenderer) {
+  const result = component.toJSON()
+  expect(result).toBeDefined()
+  expect(result).not.toBeInstanceOf(Array)
+  return result as ReactTestRendererJSON
+}
+
+/** 
+ * Hack to make async effects affect render
+ * @see https://stackoverflow.com/a/70926194
+ */
+const renderWithEffects = async (element: React.ReactElement) => {
+  let root: ReactTestRenderer = null!
+
+  await act(async () => {
+    root = create(element)
+  })
+
+  return root
+}
+
+describe('XR', () => {
+  let xrSystemMock = new XRSystemMock()
+  beforeEach(() => {
+    navigator.xr = xrSystemMock
+  })
+
+  afterEach(() => {
+    delete navigator.xr
+    vi.restoreAllMocks()
+  })
+
+  it('should render unsupported button if navigator.xr is not present', async () => {
+    delete navigator.xr
+    const root = await renderWithEffects(<XRButton mode="VR" />)
+    const tree = toJson(root)
+    expect(tree).toMatchInlineSnapshot(`
+      <button>
+        VR unsupported
+      </button>
+    `)
+  })
+
+  it('should render unsupported button if navigator.xr is present but isSessionSupported returns false for vr', async () => {
+    xrSystemMock.isSessionSupported.mockResolvedValueOnce(false)
+    const root = await renderWithEffects(<XRButton mode="VR" />)
+    const tree = toJson(root)
+    expect(tree).toMatchInlineSnapshot(`
+      <button>
+        VR unsupported
+      </button>
+    `)
+  })
+
+  it('should render enter vr button if navigator.xr is present and isSessionSupported returns true for vr', async () => {
+    xrSystemMock.isSessionSupported.mockResolvedValueOnce(true)
+    const root = await renderWithEffects(<XRButton mode="VR" />)
+    const tree = toJson(root)
+    expect(tree).toMatchInlineSnapshot(`
+      <button
+        onClick={[Function]}
+      >
+        Enter VR
+      </button>
+    `)
+  })
+
+  it('should render enter ar button if navigator.xr is present and isSessionSupported returns true for ar', async () => {
+    xrSystemMock.isSessionSupported.mockResolvedValueOnce(true)
+    const root = await renderWithEffects(<XRButton mode="AR" />)
+    const tree = toJson(root)
+    expect(tree).toMatchInlineSnapshot(`
+      <button
+        onClick={[Function]}
+      >
+        Enter AR
+      </button>
+    `)
+  })
+})
