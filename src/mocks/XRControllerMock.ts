172257c0820835980fa2f211267397307ebc67b4
Michael Bashurov <accounts@saitonakamura.me>
2023-03-23 20:11:16 +0200

    test: Added mock for XRController
    had to make private XRController fields public because of weird Typescript behaviour:
    implementing a class requires to implement private fields, but now adequate way to do so

---
+++ src/mocks/XRControllerMock.ts
@@ -0,0 +1,50 @@
+import { XRController, XRControllerEvent } from '@react-three/xr'
+import { Group, XRTargetRaySpace, XRGripSpace, XRHandSpace, Vector3, XRHandInputState, XRHandJoints } from 'three'
+
+export class XRTargetRaySpaceMock extends Group implements XRTargetRaySpace {
+  readonly angularVelocity: Vector3 = new Vector3()
+  hasAngularVelocity = false
+  hasLinearVelocity = false
+  readonly linearVelocity: Vector3 = new Vector3()
+}
+
+export class XRGripSpaceMock extends Group implements XRGripSpace {
+  readonly angularVelocity: Vector3 = new Vector3()
+  hasAngularVelocity = false
+  hasLinearVelocity = false
+  readonly linearVelocity: Vector3 = new Vector3()
+}
+
+export class XRHandSpaceMock extends Group implements XRHandSpace {
+  readonly inputState: XRHandInputState = { pinching: false }
+  readonly joints: Partial<XRHandJoints> = {}
+}
+
+export class XRControllerMock extends Group implements XRController {
+  readonly controller: XRTargetRaySpace
+  readonly grip: XRGripSpace
+  readonly hand: XRHandSpace
+  readonly index: number
+
+  // TODO Implement mocks for inputSource
+  // @ts-ignore
+  inputSource: XRInputSource
+
+  constructor(index: number) {
+    super()
+    this.index = index
+    this.controller = new XRTargetRaySpaceMock()
+    this.grip = new XRGripSpaceMock()
+    this.hand = new XRHandSpaceMock()
+  }
+
+  _onConnected(_event: XRControllerEvent): void {
+    throw new Error('Method not implemented.')
+  }
+
+  _onDisconnected(_event: XRControllerEvent): void {
+    throw new Error('Method not implemented.')
+  }
+
+  dispose(): void {}
+}
