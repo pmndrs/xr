99a13f8ee816e442306072e9e7e21fd5904407be
Michael Bashurov <accounts@saitonakamura.me>
2023-07-06 14:06:36 +0300

    fix(examples): removed alias in examples
    * alias only means to be used for local runs and we use main vite config for it, and the vite config in examples is for codesandbox

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), vanillaExtractPlugin()],
-  resolve: {
-    alias: {
-      '@react-three/xr': path.resolve(__dirname, '../src')
-    }
-  }
+  plugins: [react(), vanillaExtractPlugin()]
 })
