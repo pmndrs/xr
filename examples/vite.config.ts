74148da174f12c103b55a0ee895b5ff27f31af6a
Michael Bashurov <accounts@saitonakamura.me>
2023-07-06 14:07:13 +0300

    fix(examples): added asset handling of gltf and hdr for examples, both in sandbox and locally

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -4,5 +4,6 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  assetsInclude: ['**/*.hdr', '**/*.gltf'],
   plugins: [react(), vanillaExtractPlugin()]
 })
