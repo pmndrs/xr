9bda2c431dae276c7d3c7ad207dee9e578870734
Michael サイトー 中村 Bashurov <accounts@saitonakamura.me>
2023-07-07 13:11:56 +0300

    docs(examples): implemented example of an object that is linked to camera (#288)

--- examples/src/demos/index.tsx
+++ examples/src/demos/index.tsx
@@ -6,5 +6,6 @@ const Player = { Component: lazy(() => import('./Player')) }
 const Text = { Component: lazy(() => import('./Text')) }
 const Hands = { Component: lazy(() => import('./Hands')) }
 const Teleport = { Component: lazy(() => import('./Teleport')) }
+const CameraLinkedObject = { Component: lazy(() => import('./CameraLinkedObject')) }
 
-export { Interactive, HitTest, Player, Text, Hands, Teleport }
+export { Interactive, HitTest, Player, Text, Hands, Teleport, CameraLinkedObject }
