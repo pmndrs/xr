30ca974703563f87497709aca75327eaed50dcda
Cody Bennett <23324155+CodyJasonBennett@users.noreply.github.com>
2023-07-13 09:27:48 -0500

    chore: fix examples

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
+import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@react-three/xr': path.resolve(__dirname, '../src')
+    }
+  },
   assetsInclude: ['**/*.hdr', '**/*.gltf'],
   plugins: [react(), vanillaExtractPlugin()]
 })
