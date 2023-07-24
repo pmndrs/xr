d6acce10d01d4245000ae49ce3e889d78f89d249
Michael サイトー 中村 Bashurov <accounts@saitonakamura.me>
2023-07-24 21:25:03 +0300

    fix(examples): fix codesandbox build (#293)

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
-import path from 'node:path'
 import react from '@vitejs/plugin-react'
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@react-three/xr': path.resolve(__dirname, '../src')
-    }
-  },
   assetsInclude: ['**/*.hdr', '**/*.gltf'],
   plugins: [react(), vanillaExtractPlugin()]
 })
