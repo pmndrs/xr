432aff6804943baa61fe042eea8916fc7432a289
Bela Bohlender <bela@bohlender.email>
2024-11-01 14:53:08 +0100

    support <XRSpace space="grib-space" /> and more

--- packages/react/xr/src/hand.tsx
+++ packages/react/xr/src/hand.tsx
@@ -37,18 +37,19 @@ export const XRHandModel = forwardRef<Object3D, XRHandModelOptions>((options, re
 })
 
 /**
+ * @deprecated use `<XRSpace space="wrist">` instead of `<XRHandJoint joint="wrist">`
  * component for placing content in the hand anchored at a specific joint such as the index finger tip
  *
  * properties
  * - `joint` is the name of the joint (e.g. `"wrist"`)
  *
  * the component allows children to be placed inside for e.g. visualizing a tooltip over the index finger tip
+ *
  */
 export const XRHandJoint = forwardRef<Object3D, { joint: XRHandJoint; children?: ReactNode }>(
   ({ joint, children }, ref) => {
-    const state = useXRInputSourceStateContext('hand')
     return (
-      <XRSpace ref={ref} space={state.inputSource.hand.get(joint)!}>
+      <XRSpace ref={ref} space={joint}>
         {children}
       </XRSpace>
     )
