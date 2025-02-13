b7fecde2deb9c32aabd2414003764cdad1acca46
Bela Bohlender <bela@bohlender.email>
2025-02-13 12:00:17 +0100

    re-export defaultApply, ... from react-three/handle

--- packages/react/handle/src/index.ts
+++ packages/react/handle/src/index.ts
@@ -1,4 +1,12 @@
 export * from './hook.js'
 export * from './component.js'
 export * from './handles/index.js'
-export { HandleStore } from '@pmndrs/handle'
+export {
+  HandleStore,
+  filterForOnePointerLeftClick,
+  filterForOnePointerRightClickOrTwoPointer,
+  defaultApply,
+  defaultMapHandlesScreenCameraApply,
+  defaultOrbitHandlesScreenCameraApply,
+  defaultScreenCameraApply,
+} from '@pmndrs/handle'
