6e22bb21cb8c2f04239e5ac9cc27637a03c82b9b
Michael Dougall <6801309+itsdouges@users.noreply.github.com>
2025-04-21 11:09:07 +1000

    chore: fix build

--- packages/react/handle/src/handles/examples/pivot.tsx
+++ packages/react/handle/src/handles/examples/pivot.tsx
@@ -1,25 +1,17 @@
-import { Canvas } from '@react-three/fiber'
 import { PivotHandles } from '../pivot.js'
-import { noEvents, PointerEvents } from '@react-three/xr'
 
 export function Default() {
   return (
-    <Canvas events={noEvents}>
-      <PointerEvents />
-      <PivotHandles>
-        <mesh />
-      </PivotHandles>
-    </Canvas>
+    <PivotHandles>
+      <mesh />
+    </PivotHandles>
   )
 }
 
 export function Disabled() {
   return (
-    <Canvas events={noEvents}>
-      <PointerEvents />
-      <PivotHandles enabled={false}>
-        <mesh />
-      </PivotHandles>
-    </Canvas>
+    <PivotHandles enabled={false}>
+      <mesh />
+    </PivotHandles>
   )
 }
--- packages/react/handle/src/handles/examples/transform.tsx
+++ packages/react/handle/src/handles/examples/transform.tsx
@@ -1,25 +1,17 @@
-import { PointerEvents, noEvents } from '@react-three/xr'
-import { Canvas } from '@react-three/fiber'
 import { TransformHandles } from '../transform.js'
 
 export function Default() {
   return (
-    <Canvas events={noEvents}>
-      <PointerEvents />
-      <TransformHandles>
-        <mesh />
-      </TransformHandles>
-    </Canvas>
+    <TransformHandles>
+      <mesh />
+    </TransformHandles>
   )
 }
 
 export function Disabled() {
   return (
-    <Canvas events={noEvents}>
-      <PointerEvents />
-      <TransformHandles enabled={false}>
-        <mesh />
-      </TransformHandles>
-    </Canvas>
+    <TransformHandles enabled={false}>
+      <mesh />
+    </TransformHandles>
   )
 }
