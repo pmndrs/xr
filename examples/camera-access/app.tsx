8ad776385042e5d14cf89c985d10cae0426c9e13
Mike Nisbet <me@mikesky.dev>
2025-07-24 10:44:25 +0100

    clone dom example to work from

---
+++ examples/camera-access/app.tsx
@@ -0,0 +1,31 @@
+import { Canvas } from '@react-three/fiber'
+import { createXRStore, XR, XRDomOverlay, XROrigin } from '@react-three/xr'
+import { useState } from 'react'
+import {} from '@react-three/drei'
+
+const store = createXRStore()
+
+export function App() {
+  const [bool, setBool] = useState(false)
+  return (
+    <>
+      <button onClick={() => store.enterAR()}>Enter AR</button>
+      <Canvas style={{ width: '100%', flexGrow: 1 }}>
+        <XR store={store}>
+          <XROrigin />
+
+          <XRDomOverlay
+            style={{ width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}
+          >
+            <div
+              style={{ backgroundColor: bool ? 'red' : 'green', padding: '1rem 2rem' }}
+              onClick={() => setBool((b) => !b)}
+            >
+              Hello World
+            </div>
+          </XRDomOverlay>
+        </XR>
+      </Canvas>
+    </>
+  )
+}
---
+++ examples/camera-access/vite.config.ts
@@ -0,0 +1,11 @@
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), basicSsl()],
+  resolve: {
+    dedupe: ['@react-three/fiber', 'three'],
+  },
+})
