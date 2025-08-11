16ec706cdcacbf54a539cea7ccf690af72a379d3
Kailean O'Keefe <kailean.okeefe@hexagon.com>
2025-08-11 09:29:17 -0600

    fix: Fixed the styling in the hit-test example

--- examples/hit-testing/index.tsx
+++ examples/hit-testing/index.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './src/app.js'
+import './style.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- examples/hit-testing/src/dom-overlay.tsx
+++ examples/hit-testing/src/dom-overlay.tsx
@@ -1,28 +1,41 @@
 import { useXRStore, XRDomOverlay } from '@react-three/xr'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useTestDistanceToFloor } from './useTestDistanceToFloor.js'
 
 export const DomOverlay = () => {
-  const [distance, setDistance] = useState<string>('')
+  const [distanceMessage, setDistanceMessage] = useState<string>('')
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null)
   const testDistanceToFloor = useTestDistanceToFloor()
   const xr_store = useXRStore()
+
+  const showMessageWithTimeout = (message: string) => {
+    setDistanceMessage(message)
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+    timeoutRef.current = setTimeout(() => {
+      setDistanceMessage('')
+      timeoutRef.current = null
+    }, 5000)
+  }
 
   const onTestDistanceToFloorClick = async (e: React.MouseEvent<HTMLButtonElement>) => {
     const distance = await testDistanceToFloor()
     console.log('distance:', distance)
-    if (distance !== undefined) {
-      setDistance(`Distance to floor: ${distance.toFixed(2)} meters`)
-      setTimeout(() => {
-        setDistance('')
-      }, 5000)
-    }
+
+    distance !== undefined
+      ? showMessageWithTimeout(`Distance to floor: ${distance.toFixed(2)} meters`)
+      : showMessageWithTimeout('Unable to calculate distance. Try looking towards the floor.')
+
     e.stopPropagation()
   }
 
   return (
     <XRDomOverlay>
-      <h1 className={'distanceMessage'}>{distance}</h1>
-      <button onClick={() => xr_store.getState().session?.end()}>Exit AR</button>
+      <span className={'distanceMessage'}>{distanceMessage}</span>
+      <button className={'exitAR'} onClick={() => xr_store.getState().session?.end()}>
+        Exit AR
+      </button>
       <button className={'testDistanceToFloor'} onClick={onTestDistanceToFloorClick}>
         Test Distance to Floor
       </button>
