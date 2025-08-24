4db0372fd4fb50dac865d4fdbccfc1eb5f736544
taeuscherpferd <ibber@myokcorral.com>
2025-08-24 10:54:19 -0600

    chore: Removed grabbing and handle logic

--- examples/hit-testing/src/duck.tsx
+++ examples/hit-testing/src/duck.tsx
@@ -8,12 +8,7 @@
  */
 import { useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import { Handle, HandleStore } from '@react-three/handle'
-import { useXRRequestHitTest } from '@react-three/xr'
-import { useRef, useState } from 'react'
 import { Group } from 'three'
-import { onResults } from './app.js'
-import { Reticle } from './reticle.js'
 
 // const MODEL = 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/duck/model.gltf'
 const MODEL = 'duck.gltf'
@@ -22,38 +17,9 @@ useGLTF.preload(MODEL)
 
 export const Duck = (props: any) => {
   const { nodes, materials } = useGLTF(MODEL) as any
-  const [isBeingGrabbed, setisBeingGrabbed] = useState(false)
-  const hitTestRefPoint = useRef<Group>(null)
-  const duckGroup = useRef<Group>(null)
-  const hitTestRequest = useXRRequestHitTest()
-  const handleRef = useRef<HandleStore<unknown>>(null)
-
-  useFrame(async (_, __, frame: XRFrame | undefined) => {
-    if (frame && isBeingGrabbed && hitTestRefPoint.current) {
-      const requestedHitTest = await hitTestRequest(hitTestRefPoint)
-      if (requestedHitTest?.results && requestedHitTest.results.length > 0 && requestedHitTest?.getWorldMatrix) {
-        onResults('duck', requestedHitTest?.results ?? [], requestedHitTest?.getWorldMatrix)
-      }
-    }
-  })
 
   return (
-    <>
-      <group ref={duckGroup} {...props} dispose={null}>
-        <Handle
-          ref={handleRef}
-          handleRef={duckGroup}
-          apply={(state) => {
-            if (state.last) {
-              setisBeingGrabbed(false)
-            } else {
-              setisBeingGrabbed(true)
-            }
-
-            duckGroup.current?.position.copy(state.current.position)
-            duckGroup.current?.quaternion.copy(state.current.quaternion)
-          }}
-        >
+      <group {...props} dispose={null}>
           <mesh
             geometry={nodes.character_duck.geometry}
             material={nodes.character_duck.material}
@@ -76,10 +42,6 @@ export const Duck = (props: any) => {
               <mesh geometry={nodes.Cube1338_2.geometry} material={materials['Black.027']} />
             </group>
           </mesh>
-          <group ref={hitTestRefPoint} />
-        </Handle>
       </group>
-      {isBeingGrabbed && <Reticle handedness="duck" />}
-    </>
   )
 }
